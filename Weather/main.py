5fcc20c429278040eb13a3c3c7c1ba1806a10bec
optimus222 <optimus222@users.noreply.github.com>
2020-01-05 08:55:33 -0500

    great commit

--- Weather/main.py
+++ Weather/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from read import *
 from write import *
+import time as timelib
 
 import weathertest as wt
 from datetime import datetime
@@ -15,23 +16,23 @@ window.title("ezWeather")
 
 #def get_digit(number, n):
 #   return number // 10**n % 10
-
-#print(datetime.utcfromtimestamp(time))
-
+#window.root.wm_attributes('-transparentcolor','black')
+print(datetime.utcfromtimestamp(time))
+'''
 local_timezone = tzlocal.get_localzone() # get pytz timezone
 local_time = datetime.fromtimestamp(time, local_timezone)
 #hour
 hour = str(local_time)[11]+ str(local_time)[12]
-date = str(local_time)[5:10]
+date = str(local_time)[5:10]'''
 
-morning_pic = tk.PhotoImage(file = "Weather/Background Pics/clear-day.png")
-night_pic = tk.PhotoImage(file = "Weather/Background Pics/clear-night.png")
+morning_pic = "Weather/Background Pics/clear-day.png"
+night_pic = "Weather/Background Pics/clear-ight.png"
 time_picture = tk.PhotoImage(file = "")
-
+'''
 if int(hour) >=6 and int(hour) < 18:
     time_picture = morning_pic
 else:
-    time_picture = night_pic
+    time_picture = night_pic'''
 
 background_image = time_picture
 background_label = tk.Label(window, image = background_image)
@@ -39,6 +40,7 @@ background_label.place(x = 0, y = 0, relwidth = 1, relheight = 1)
 
 #read part
 title = tk.Label(window, text = "What is the (city, state) you're in? ")
+
 title.grid(column = 0, row = 0)
 
 txt = tk.Entry(window, width = 10)
@@ -53,6 +55,9 @@ no_pre = tk.PhotoImage(file = "Weather/no precipitation.png")
 yes_pre = tk.PhotoImage(file = "Weather/rainy.png")
 prec_image = tk.PhotoImage(file = "")
 
+class fcc():
+    pass
+
 def calculate():
     read = []
     read = goread(str(txt.get()))
@@ -66,6 +71,12 @@ def calculate():
     else:
         prec_image = no_pre
     prec_value.configure(image = prec_image)
+    time_picture.configure(file =  "Weather/Background Pics/{}.png".format(fcc.icon))
+    #hour = str(timelib.strftime("%H", timelib.gmtime(time)))
+    '''if int(hour) >=6 and int(hour) < 18:
+        time_picture = morning_pic
+    else:
+        time_picture = night_pic'''
    
 
 enter = tk.Button(window, width = 4, height = 1, text = "Enter", command = calculate)
@@ -73,7 +84,7 @@ enter.grid(column = 2, row = 0)
 
 
 quote = tk.Label(window, bg = "pink", text = 'Daily Quote: ' + '"Success is a journey not a destination"' + '\n' + '-Ben Sweetland', font = ("Caveat", 20))
-quote.grid(columnspan = 8, row = 8)
+quote.grid(columnspan = 8, row = 40)
 
 #write part
 coldP = tk.PhotoImage(file = "Weather/too cold.png")
@@ -89,7 +100,7 @@ thank.grid(column = 0, row = 18)
 
 def feedback5(something):
     gowrite(something)
-    thank.configure(text = "Thank you for your feedback!" + something)
+    thank.configure(text = "Thank you for your feedback! It's " + something)
 
 coldB = tk.Button(window, width = 50, height = 50, image = coldP, command = lambda: feedback5("Too Cold"))
 rightB = tk.Button(window, width = 50, height = 50, image = rightP, command = lambda: feedback5("Just Right"))
@@ -107,11 +118,16 @@ date1.grid(column = 0, row = 10)
 temp1.grid(column = 2, row = 10)
 prec1.grid(column = 4, row = 10)
 
-date_value = tk.Label(window, text = date, font = ("none", 30))
+date_value = tk.Label(window, text = str(timelib.strftime("%m-%d", timelib.gmtime(time))), font = ("none", 30))
 temp_value = tk.Label(window, text = str(temperature) + " degree", font = ("none", 30))
 prec_value = tk.Label(window, image = prec_image)
 date_value.grid(column = 0, row = 11)
 temp_value.grid(column = 2, row = 11)
 prec_value.grid(column = 4, row = 11)
+
+time12345 = tk.Label(window, text = "Time: ", font = ("none", 15))
+time1234 = tk.Label(window, text = str(timelib.strftime("%H:%M", timelib.gmtime(time))), font = ("none", 15))
+time12345.grid(column = 0, row = 20)
+time1234.grid(column = 0, row = 21)
 
 window.mainloop()
