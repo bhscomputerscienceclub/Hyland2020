c4b844d3a49633a47e6c7b52aed2c19d977e0da4
optimus222 <optimus222@users.noreply.github.com>
2020-01-05 02:07:16 -0500

    Update frontend

--- Weather/main.py
+++ Weather/main.py
@@ -4,15 +4,20 @@ from write import *
 
 window = tk.Tk()
 window.geometry('750x1334')
+window.configure(background='white')
 window.title("ezWeather")
 
+quote = tk.Label(window, bg = "pink", text = '"Success is a journey not a destination"' + '\n' + '-Ben Sweetland')
+quote.grid(column = 0, row = 8)
+
+#read part
 title = tk.Label(window, text = "What is the (city, state) you're in? ")
 title.grid(column = 0, row = 0)
 
 txt = tk.Entry(window, width = 10)
 txt.grid(column = 1, row = 0)
 
-tip1 = tk.Label(window, text="")
+tip1 = tk.Label(window, text="", font = ("roboto", 10))
 tip1.grid(column = 0, row = 1)
 
 def calculate():
@@ -24,7 +29,7 @@ def calculate():
 enter = tk.Button(window, width = 4, height = 1, text = "Enter", command = calculate)
 enter.grid(column = 2, row = 0)
 
-
+#write part
 coldP = tk.PhotoImage(file = "Weather/too cold.png")
 rightP = tk.PhotoImage(file = "Weather/just right.png")
 hotP = tk.PhotoImage(file = "Weather/too hot.png")
@@ -47,5 +52,19 @@ coldB.grid(column = 1, row = 5)
 rightB.grid(column = 2, row = 5)
 hotB.grid(column = 3, row = 5)
 
+# date temperature and precipitation
+date1 = tk.Label(window, text = "Date", font = ("none", 20))
+temp1 = tk.Label(window, text = "Temperature", font = ("none", 20))
+prec1 = tk.Label(window, text = "Precipitation", font = ("none", 20))
+date1.grid(column = 0, row = 10)
+temp1.grid(column = 2, row = 10)
+prec1.grid(column = 4, row = 10)
+
+date_value = tk.Label(window, text = "x", font = ("none", 30))
+temp_value = tk.Label(window, text = "y degrees", font = ("none", 30))
+prec_value = tk.Label(window, text = "x ", font = ("none", 30))
+date_value.grid(column = 0, row = 11)
+temp_value.grid(column = 2, row = 11)
+prec_value.grid(column = 4, row = 11)
  
 window.mainloop()
